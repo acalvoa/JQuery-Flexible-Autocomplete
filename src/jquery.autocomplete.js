f109444a8916c033d61808e69423119ae1418bfd
Angelo Calvo Alfaro <angelo.calvoa@gmail.com>
2015-08-04 12:41:20 -0300

    SE INTEGRA LA LIBRERIA EN SU TOTALIDAD

--- src/jquery.autocomplete.js
+++ src/jquery.autocomplete.js
@@ -9,11 +9,13 @@
             onBlur: function(e){},
             dataAction: function(e,handler){},
             handler: function(data){},
-            maxVisible: 5,
-            css: {},
+            maxVisible: 10,
             content: null,
-            cache: {},
-            minLength: 1
+            minLength: 1,
+            suggestHeight: 30,
+            suggestArray: [],
+            index:0,
+            selected: null
         };
         return this.each(function() {
             //ESTRTCTURA DE SINCRONIZACION
@@ -38,6 +40,7 @@
                         SYNC_STRUCT._QUEUED.splice(0,1);
                         action.handler.apply(this,action.arg);
                     }
+                    SYNC_STRUCT._BUSY = !SYNC_STRUCT._BUSY;
                     return;
                 },
                 _FREE: function(){
@@ -53,6 +56,11 @@
                     if(SYNC_STRUCT._QUEUED > 0){
                         SYNC_STRUCT._UNQUEUED();
                     }
+                },
+                _FREEALL: function(){
+                    SYNC_STRUCT._BUSY = false;
+                    SYNC_STRUCT._ALLOCATE = 1;
+                    SYNC_STRUCT._QUEUED = [];
                 },
                 _FFUNC: function(func){
                     var func_txt = func.toString();
@@ -80,62 +88,120 @@
                     _SETTINGS.dataAction = SYNC_STRUCT._FFUNC(_SETTINGS.dataAction);
                 //ASIGNAMOS LOS HANDLER DE EVENTOS
                 $(_SELF).on('change',function(e,free){
-                    // _SETTINGS.onChange(e,free);
+                    SYNC_STRUCT._DO(_SETTINGS.onChange,e,SYNC_STRUCT);
                 });
                 $(_SELF).on('keyup',function(e){
-                    SYNC_STRUCT._DO(_SETTINGS.onKeyup,e,SYNC_STRUCT);
-                    SYNC_STRUCT._DO(_SETTINGS.dataAction,e,PRIV.handler);
+                    if(e.keyCode == 38 || e.keyCode == 40 || e.keyCode == 13){
+                        e.preventDefault();
+                        PRIV._ANALIZE_MOV(e.keyCode);
+                    }
+                    else
+                    {
+                        SYNC_STRUCT._DO(_SETTINGS.onKeyup,e,SYNC_STRUCT);
+                        _SETTINGS.index = -1;
+                        _SETTINGS.suggestArray = [];
+                        _SETTINGS.content = $(_SELF).val();
+                        SYNC_STRUCT._DO(_SETTINGS.dataAction,e,PRIV._HANDLER,SYNC_STRUCT);
+                    }
                 });
                 $(_SELF).on('focus',function(e,free){
-                    // _SETTINGS.onFocus(e,free);
+                     SYNC_STRUCT._DO(_SETTINGS.onFocus,e,SYNC_STRUCT);
+                     SYNC_STRUCT._DO(PRIV._FOCUS,e,SYNC_STRUCT);
                 });
                 $(_SELF).on('blur',function(e,free){
-                    // _SETTINGS.onBlur(e,free);
+                    SYNC_STRUCT._DO(_SETTINGS.onBlur,e,SYNC_STRUCT);
+                    SYNC_STRUCT._DO(PRIV._NOT_FOCUS,e,SYNC_STRUCT);
                 });
             };
             //DEFINIMOS LOS METODOS PRIVADOS
             var PRIV = {
-                handler: function(data,response){
-                    PRIV.display(data,response,SYNC_STRUCT);
+                _HANDLER: function(data,response){
+                    PRIV.display(data,SYNC_STRUCT);
                 },
-                display: function(data,response,SYNC_STRUCT){
+                display: function(data,SYNC_STRUCT){
                     //CARGAMOS POSICICONES RELATIVAS AL ELEMENTO
                     var offset = $(_SELF).offset();
                     var width = $(_SELF).css('width').replace("px","");
                     var height = $(_SELF).css('height').replace("px","");
-                    //CCREAMOS EL BOJETO CON LAS VARIABLES CSS
+                    //CCREAMOS EL OBJETO CON LAS VARIABLES CSS
                     var auto_css = {
-                        position: "absolute",
                         top: (offset.top + parseInt(height)),
                         left: (offset.left - 1),
-                        background: "#FFF",
-                        border: "1px solid #CCC",
                         width: parseInt(width),
-                        height: "auto",
-                        "z-index":9999
+                        "max-height": (_SETTINGS.suggestHeight*_SETTINGS.maxVisible)
                     };
                     //CREAMOS EL ELEMENTO
+                    $(".autocomplete-handler").remove();
                     var container = $("<div></div>",{
                         class: "autocomplete-handler" 
                     }).appendTo($('body')).css(auto_css);
-                    // CREAMOS NUEVAS EXPRESIONES
-                    $("<div>dede</div>").appendTo(container).css({
-                        height: "20px",
-                        padding: "5px 15px",
-                        "white-space": "nowrap"
-                    }).on('mouseenter',function(){
-                        $(this).css({
-                            background: "#EEE"
-                        })
-                    }).on('mouseleave',function(){
-                        $(this).css({
-                            background: "#FFF"
-                        })
-                    }).on('click',function(){
-
-                    });
+                    //INICIAMOS LOS CAMPOS INTEGRADOS
+                    var elements = 0;
+                    for(i=0;i<data.length;i++){
+                        var p = function(data,i){
+                            // CREAMOS NUEVAS EXPRESIONES
+                            var regex = new RegExp(_SETTINGS.content, "gi");
+                            if(regex.test(data[i].label) && _SETTINGS.content != ""){
+                                elements++;
+                                var datastr = data[i].label.replace(regex,"<strong>"+_SETTINGS.content+"</strong>").toUpperCase();
+                                var sug = $("<div>"+datastr+"</div>",{
+                                    "data-id": i
+                                }).css('height',(_SETTINGS.suggestHeight-10)).addClass('autocomplete-suggest').on('click',function(){
+                                    $(_SELF).val(data[i].data);
+                                    container.remove();
+                                }).on('mouseenter',function(){
+                                    _SETTINGS.index = -1;
+                                    PRIV._RESET_SELECTED(sug);
+                                }).on('mouseleave',function(){
+                                    PRIV._RESET_SELECTED(null);
+                                }).appendTo(container);
+                                _SETTINGS.suggestArray.push(sug);
+                            }
+                            return; 
+                        }
+                        p(data,i);
+                    }
+                    if(elements > 10){
+                        container.css("overflow-y","scroll");
+                    }
+                    else if(elements == 0){
+                        $(".autocomplete-handler").remove();
+                    }
                     //LIBERAMOS EL SEGURO
                     SYNC_STRUCT._FREE();
+                },
+                _ANALIZE_MOV: function(code){
+                    if(_SETTINGS.suggestArray.length == 0) return;
+                    if(code == 13)
+                    {
+                        if(_SETTINGS.selected == null) return;
+                        _SETTINGS.selected.trigger('click');
+                    }
+                    else if(code == 38){
+                        //UP BOTTON
+                        if(_SETTINGS.index <= -1) return;
+                        PRIV._RESET_SELECTED(_SETTINGS.suggestArray[--_SETTINGS.index]);
+                    }
+                    else if(code == 40)
+                    {
+                        //DOWN BOTTON
+                        if(_SETTINGS.index >= _SETTINGS.suggestArray.length) return;
+                        PRIV._RESET_SELECTED(_SETTINGS.suggestArray[++_SETTINGS.index]);
+                    }
+                },
+                _RESET_SELECTED: function(SELECTED){
+                    if(_SETTINGS.selected != null) _SETTINGS.selected.css('background','#FFF');
+                    _SETTINGS.selected = SELECTED;
+                    _SETTINGS.selected.css('background',"#EEE");
+                },
+                _NOT_FOCUS: function(e,SYNC_STRUCT){
+                    $(".autocomplete-handler")  .remove();
+                    SYNC_STRUCT._FREEALL();
+                },
+                _FOCUS: function(e,SYNC_STRUCT){
+                    _SETTINGS.index = -1;
+                    _SETTINGS.suggestArray = [];
+                    SYNC_STRUCT._DO(_SETTINGS.dataAction,e,PRIV._HANDLER,SYNC_STRUCT);
                 }
             }
             //DEFINIMOS METODOS PUBLICOS
